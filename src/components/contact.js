1e3945db29c38c4dc55de64cf3c80e669579df0d
Tangni Wang <twang63@ucsc.edu>
2019-09-19 23:49:11 -0700

    contact page

--- src/components/contact.js
+++ src/components/contact.js
@@ -3,26 +3,26 @@ import React, { Component } from "react";
 class Contact extends Component {
   render() {
     return (
-      // <form name="contact" method="POST" data-netlify="true">
-      //   <p>
-      //     <label>
-      //       Your Name: <input type="text" name="name" />
-      //     </label>
-      //   </p>
-      //   <p>
-      //     <label>
-      //       Your Email: <input type="email" name="email" />
-      //     </label>
-      //   </p>
-      //   <p>
-      //     <label>
-      //       Message: <textarea name="message"></textarea>
-      //     </label>
-      //   </p>
-      //   <p>
-      //     <button type="submit">Send</button>
-      //   </p>
-      // </form>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
       <h1>hahahah</h1>
     );
   }
