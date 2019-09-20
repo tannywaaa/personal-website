596cfb7fd822bcc1897ee3bc8af0d6291c4f164d
Tangni Wang <twang63@ucsc.edu>
2019-09-19 23:28:19 -0700

    contact delete

--- src/components/contact.js
+++ src/components/contact.js
@@ -16,15 +16,6 @@ class Contact extends Component {
         </p>
         <p>
           <label>
-            Your Role:{" "}
-            <select name="role[]" multiple>
-              <option value="leader">Leader</option>
-              <option value="follower">Follower</option>
-            </select>
-          </label>
-        </p>
-        <p>
-          <label>
             Message: <textarea name="message"></textarea>
           </label>
         </p>
