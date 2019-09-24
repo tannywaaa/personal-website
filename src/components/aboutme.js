ec389ed92f803283a70374dc33d2fc4bc504a6d8
Tangni Wang <twang63@ucsc.edu>
2019-09-24 15:52:51 -0700

    about me button added

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 import Profile from "../img/profileme.png";
 import { Badge } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 
 class About extends Component {
   render() {
@@ -10,7 +11,7 @@ class About extends Component {
         <div style={{ background: "#DAD4DB", color: "black" }}>
           <Grid>
             <Cell col={5}>
-              <img src={Profile} className="me-img" />
+              <img src={Profile} alt="avatar" className="me-img" />
             </Cell>
             <Cell col={7}>
               <div className="about-text">
@@ -40,17 +41,42 @@ class About extends Component {
                   graduated from University of California, Santa Cruz, majoring
                   in Computer Science. Strangers think I'm quiet. Friends think
                   I'm outgoing. My best friend thinks I'm crazy.
-                </p>
-                <p>
-                  I am very desperate for a job in my major related fields. So
-                  if you are a recuiter or you know somone that needs a hard
-                  working and not too crazy girl to make you team or your
+                  <br></br>
+                  <br></br>I am very desperate for a job in my major related
+                  fields. So if you are a recuiter or you know somone that needs
+                  a hard working and not too crazy girl to make you team or your
                   company a little better, PLEASE PLEASE click on the button
                   below to check out my resume and contact me. I am laying on
                   the couch all day just to wait for your call and email. If you
                   want to make my day better and possibly make your day better
                   too, hire me!
                 </p>
+                <br></br>
+                <Button
+                  variant="outline-danger"
+                  href="https://www.dropbox.com/s/04nnw2wtj59e7kn/spring_resume-7.pdf?dl=0"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  style={{
+                    width: "30%",
+                    fontSize: " 20px",
+                    padding: "10px",
+                    marginRight: "30%"
+                  }}
+                >
+                  Hire Me
+                </Button>
+                <Button
+                  variant="outline-danger"
+                  href="/contact"
+                  style={{
+                    width: "30%",
+                    fontSize: " 20px",
+                    padding: "10px"
+                  }}
+                >
+                  Contact
+                </Button>
               </div>
             </Cell>
           </Grid>
