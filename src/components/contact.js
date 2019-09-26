9ce0b7a5a5c66812b19fd6eebf5b2024ad5fdf82
Tangni Wang <twang63@ucsc.edu>
2019-09-26 16:55:44 -0700

    contact mobile fix

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 import Profile2 from "../img/profile2.png";
+import Mobile from "../img/contact-mobile.png";
 
 class Contact extends Component {
   constructor() {
@@ -76,47 +77,37 @@ class Contact extends Component {
     return (
       <div
         className="contact-body"
-        style={{ background: "#DAD4DB", color: "black", paddingTop: "5%" }}
+        style={{ background: "#DAD4DB", color: "black", paddingTop: "15%" }}
       >
         <Grid className="contact-grid">
-          <Cell col={12}>
-            <h2>Tangni Wang</h2>
-            <br></br>
-            <img src={Profile2} alt="avatar" style={{ height: "200px" }} />
-          </Cell>
-          <Cell col={12}>
-            <br></br>
-            <br></br>
-            <br></br>
-            <h2>Contact Me</h2>
-            <br></br>
-            <hr />
+          <h2>CONTACT ME</h2>
+          <br></br>
+          <img src={Mobile} alt="avatar" style={{ height: "200px" }} />
 
-            <div className="contact-list">
-              <List>
-                <ListItem>
-                  <ListItemContent className="contact-text">
-                    <i className="fas fa-phone-volume" aria-hidden="true" />
-                    (831) 419-3981
-                  </ListItemContent>
-                </ListItem>
-                <br></br>
-                <ListItem>
-                  <ListItemContent className="contact-text">
-                    <i className="far fa-envelope" aria-hidden="true" />
-                    wangtangni96@gmail.com
-                  </ListItemContent>
-                </ListItem>
-                <br></br>
-                <ListItem>
-                  <ListItemContent className="contact-text">
-                    <i className="fab fa-skype" aria-hidden="true" />
-                    wangtangni96
-                  </ListItemContent>
-                </ListItem>
-              </List>
-            </div>
-          </Cell>
+          <div className="contact-list-mobile">
+            <List>
+              <ListItem>
+                <ListItemContent className="contact-text-mobile">
+                  <i className="fas fa-phone-volume" aria-hidden="true" />
+                  (831) 419-3981
+                </ListItemContent>
+              </ListItem>
+              <br></br>
+              <ListItem>
+                <ListItemContent className="contact-text-mobile">
+                  <i className="far fa-envelope" aria-hidden="true" />
+                  wangtangni96@gmail.com
+                </ListItemContent>
+              </ListItem>
+              <br></br>
+              <ListItem>
+                <ListItemContent className="contact-text-mobile">
+                  <i className="fab fa-skype" aria-hidden="true" />
+                  wangtangni96
+                </ListItemContent>
+              </ListItem>
+            </List>
+          </div>
         </Grid>
       </div>
     );
