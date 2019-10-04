2eac032680bc402b24a0b106e3b211e893f2fe44
tannywaaa <twang63@ucsc.edu>
2019-10-03 17:43:47 -0700

    Update aboutme.js

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -60,7 +60,7 @@ class About extends Component {
                   of California, Santa Cruz and received my bachelors in Computer Science.
                   Strangers think I'm quiet. Friends think I'm outgoing. My best
                   friend thinks I'm crazy.
-                  <br></br>I am seeking a stimulating and challenging work environment
+                  <br></br><br></br>I am seeking a stimulating and challenging work environment
                   where I can utilize my passion and software development skills
                   to advance my employer's objectives and build my knowledge of
                   software technologies and applications. If you are a recuiter
@@ -130,17 +130,16 @@ class About extends Component {
                 </Badge>
               </h2>
               <p>
-                Welcome to the least boring page. My name is Tangni Wang but I
-                go by Tanny as well. I just recently graduated from University
-                of California, Santa Cruz, majoring in Computer Science.
-                Strangers think I'm quiet. Friends think I'm outgoing. My best
-                friend thinks I'm crazy. I seek a stimulating and challenging
-                work environment where I can utilize my passion and software
-                development skills to advance my employer's objectives and build
-                my knowledge of software technologies and applications. If you
-                are a recuiter or you know somone that needs a passionate, hard
-                working and not too crazy girl to join your team, feel free to
-                click on the button below to check out my resume and contact me.
+                Welcome to the least boring page. My name is Tangni Wang, but I
+                  go by Tanny. I recently graduated from the University
+                  of California, Santa Cruz and received my bachelors in Computer Science.
+                  Strangers think I'm quiet. Friends think I'm outgoing. My best
+                  friend thinks I'm crazy. I am seeking a stimulating and challenging work environment
+                  where I can utilize my passion and software development skills
+                  to advance my employer's objectives and build my knowledge of
+                  software technologies and applications. If you are a recuiter
+                  or you know somone that needs a passionate and hard working person to join your team, feel free to click on
+                  the button below to check out my resume and contact me.
               </p>
             </div>
             <Button
